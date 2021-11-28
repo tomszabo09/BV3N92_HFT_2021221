e47102f0cc81d9a162ffac947fdf5e1c7739d804
tomt09 <tom.szabo09@gmail.com>
2021-11-28 16:01:25 +0100

    removed default comments

--- BV3N92_HFT_2021221.Endpoint/Startup.cs
+++ BV3N92_HFT_2021221.Endpoint/Startup.cs
@@ -16,8 +16,6 @@ namespace BV3N92_HFT_2021221.Endpoint
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -33,7 +31,6 @@ namespace BV3N92_HFT_2021221.Endpoint
             services.AddSingleton<DbContext, ParliamentAdministrationDbContext>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())

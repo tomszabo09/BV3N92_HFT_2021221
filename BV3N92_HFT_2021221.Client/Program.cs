2009510dbd3f5f5fb947c3ea85e737873dda6eb4
tomt09 <tom.szabo09@gmail.com>
2021-11-28 16:01:47 +0100

    various api call tests

--- BV3N92_HFT_2021221.Client/Program.cs
+++ BV3N92_HFT_2021221.Client/Program.cs
@@ -17,6 +17,47 @@ namespace BV3N92_HFT_2021221.Client
             //    RulingParty = "Party of Tests"
             //}, "parliament");
 
+            //rest.Post<Party>(new Party()
+            //{
+            //    ParliamentID = 2,
+            //    PartyName = "Test Party",
+            //    Ideology = Ideologies.Conservative.ToString()
+            //}, "party");
+
+            //rest.Post<PartyMember>(new PartyMember()
+            //{
+            //    LastName = "TestName",
+            //    Age = 50,
+            //    PartyID = 5
+            //}, "partymember");
+
+            //rest.Delete(4, "parliament");
+            //rest.Delete(3, "party");
+            //rest.Delete(3, "partymember");
+
+            //rest.Put<Parliament>(new Parliament()
+            //{
+            //    ParliamentID = 1,
+            //    ParliamentName = "Bundestag",
+            //    RulingParty = "NSDAP"
+            //}, "parliament");
+
+            //rest.Put<Party>(new Party()
+            //{
+            //    PartyID = 4,
+            //    ParliamentID = 3,
+            //    PartyName = "Hungarian Conservative Party",
+            //    Ideology = Ideologies.Conservative.ToString()
+            //}, "party");
+
+            //rest.Put<PartyMember>(new PartyMember()
+            //{
+            //    MemberID = 6,
+            //    LastName = "von Schacht",
+            //    Age = 24,
+            //    PartyID = 7
+            //}, "partymember");
+
             var rulingparties = rest.Get<Party>("stat/getallrulingparties");
             var shortnamedmembers = rest.GetById<PartyMember>(1, "stat/getshortnamedmembers");
             var seniormembers = rest.GetById<PartyMember>(1,"stat/getseniormembers");

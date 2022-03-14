b40ed41306c629d2f687c8fc0809a494832c8d8b
tomt09 <tom.szabo09@gmail.com>
2022-03-14 15:33:31 +0100

    added wpf client

---
+++ BV3N92_GUI_2021222.WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using BV3N92_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BV3N92_GUI_2021222.WpfClient
+{
+    public class MainWindowViewModel
+    {
+        public RestCollection<Parliament> Parliaments { get; set; }
+        public RestCollection<Party> Parties { get; set; }
+        public RestCollection<PartyMember> PartyMembers { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Parliaments = new RestCollection<Parliament>("http://localhost:41126/", "parliament");
+            Parties = new RestCollection<Party>("http://localhost:41126/", "party");
+            PartyMembers = new RestCollection<PartyMember>("http://localhost:41126/", "partymember");
+        }
+    }
+}
